f3f22aa0862c6223365ecb96850809e3ec7a0a53
Thomas Gauthier-Caron <thomas.gauthier.c@gmail.com>
2020-04-24 20:35:54 -0400

    generate username if none provided

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,5 +1,5 @@
 import Room from "./Room";
-
+import randomStringGenerator from "../utils/randomStringGenerator";
 let users = 1;
 
 class RoomSystem {
@@ -10,30 +10,44 @@ class RoomSystem {
     this.io = io;
 
     this.io.on("connection", (socket: SocketIO.Socket) => {
-      socket.on("joinRoom", async ({ room: roomId, username, password } : {username:string, room:string, password:string}, clb) => {
-        socket.join(roomId);
+      socket.on(
+        "joinRoom",
+        async (
+          {
+            room: roomId,
+            username,
+            password,
+          }: { username: string; room: string; password: string },
+          clb
+        ) => {
+          socket.join(roomId);
 
-        const room: Room = await new Promise((resolve, reject) => {
-          let roomm = this.rooms.get(roomId);
-          if (roomm) {
-            resolve(roomm);
-          } else {
-            roomm = new Room(roomId, "password", io, () => {
-              roomCreationCallback(roomm);
-              resolve(roomm);
-            });
+          if (!username) {
+            username = randomStringGenerator();
           }
-        });
 
-        this.rooms.set(roomId, room);
+          const room: Room = await new Promise((resolve, reject) => {
+            let room = this.rooms.get(roomId);
+            if (room) {
+              resolve(room);
+            } else {
+              room = new Room(roomId, "password", io, () => {
+                roomCreationCallback(room);
+                resolve(room);
+              });
+            }
+          });
 
-        try {
-          await room.join(socket, username, password);
-          if (clb) clb({ status: 200 });
-        } catch {
-          if (clb) clb({ status: 401 });
+          this.rooms.set(roomId, room);
+
+          try {
+            await room.join(socket, username, password);
+            if (clb) clb({ status: 200 });
+          } catch {
+            if (clb) clb({ status: 401 });
+          }
         }
-      });
+      );
     });
   }
 }
---
+++ src/utils/randomStringGenerator.ts
@@ -0,0 +1,5 @@
+const { generateCombination } = require("gfycat-style-urls")
+
+export default function () : string {
+    return generateCombination(2, "", true);
+}
